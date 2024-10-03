55280314c06c300d42ae06c2eb338ba4092c6dca
yllNapoleoni <yllaimbontaparta@gmail.com>
2024-10-03 16:17:20 +0200

    hello

---
+++ js/main.js
@@ -0,0 +1,24 @@
+var x = 10
+
+var b = 30 - 100
+
+console.log('hello laskjdgh')
+
+console.log(x)
+
+console.log(b)
+
+b+=15
+console.group(a)
+
+b++,
+
+console.log(b)
+
+var numri1 = "20"
+var numri2 = 20
+
+console.log(numri1 == numri2)
+console.log(numri1 === numri2)
+
+console.log(!(numri1 == numri2))
