5ac5222a3ebdda6bb12fe68ae6b08a1b5525d97a
yllNapoleoni <yllaimbontaparta@gmail.com>
2024-10-08 16:03:31 +0200

    mfal

---
+++ moduli27/js/main.js
@@ -0,0 +1,11 @@
+var a = 10
+var b=6
+var c=15
+var d=19
+console.log(a>b && c<d)
+console.log(a<c || a==b)
+console.log(!(a>b))
+
+var school="zenel hajdini"
+
+console.log("my school name is " + school)
