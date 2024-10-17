35a9dace7c52d70616eed455f58f4b6c4555bcc6
yllNapoleoni <yllaimbontaparta@gmail.com>
2024-10-17 07:18:19 -0700

    asdlkjgn

---
+++ moduli29/js/main.js
@@ -0,0 +1,13 @@
+var program=["1","2","3","4","5","6","7","8","9","10"];
+console.log(program[4]);
+program.push('1000');
+console.log(program);
+program.pop();
+
+program.unshift('ylliu');
+
+program.shift();
+
+program.splice(1,0, "huanger");
+
+console.log(program);
