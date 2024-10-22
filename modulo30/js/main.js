ead79a842929b36898b15eb78505132cf8927a23
yllNapoleoni <yllaimbontaparta@gmail.com>
2024-10-22 07:18:44 -0700

    asljdgh

---
+++ modulo30/js/main.js
@@ -0,0 +1,17 @@
+var age =15
+var city ='prishtina'
+
+if (age >=18 && city=='prishtina'){
+    console.log('mnesh me vbotu')
+}
+else{
+    console.log('smunesh me votu')
+}
+var cars=['bmw','mercedews','lexus','audi','buggati','mustang','corvette'];
+
+if (cars[4]=='corvette'){
+    console.log('its mot')
+}
+else{
+    console.log('it is')
+}
